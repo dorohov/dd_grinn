2b4f018971edff951e3dec0213cb2f6530cfc099
Pavel Proyavin <pavel@dorohovdesign.com>
2019-05-23 17:55:37 +0300

    Changelog from 230519

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,12 @@
 (function($) {
     "use strict"
     $(function() {
-        
+        $('.__dd__slide__bottom__carousel__items').slick({
+            centerMode: true,
+            centerPadding: '20%',
+            slidesToShow: 1,
+            arrows: false
+        })
     })
 })(jQuery);
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24oJCkge1xyXG4gICAgXCJ1c2Ugc3RyaWN0XCJcclxuICAgICQoZnVuY3Rpb24oKSB7XHJcbiAgICAgICAgXHJcbiAgICB9KVxyXG59KShqUXVlcnkpOyJdfQ==
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiKGZ1bmN0aW9uKCQpIHtcclxuICAgIFwidXNlIHN0cmljdFwiXHJcbiAgICAkKGZ1bmN0aW9uKCkge1xyXG4gICAgICAgICQoJy5fX2RkX19zbGlkZV9fYm90dG9tX19jYXJvdXNlbF9faXRlbXMnKS5zbGljayh7XHJcbiAgICAgICAgICAgIGNlbnRlck1vZGU6IHRydWUsXHJcbiAgICAgICAgICAgIGNlbnRlclBhZGRpbmc6ICcyMCUnLFxyXG4gICAgICAgICAgICBzbGlkZXNUb1Nob3c6IDEsXHJcbiAgICAgICAgICAgIGFycm93czogZmFsc2VcclxuICAgICAgICB9KVxyXG4gICAgfSlcclxufSkoalF1ZXJ5KTsiXX0=
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
 (function($) {
     "use strict"
     $(function() {
-        
+        $('.__dd__slide__bottom__carousel__items').slick({
+            centerMode: true,
+            centerPadding: '20%',
+            slidesToShow: 1,
+            arrows: false
+        })
     })
 })(jQuery);
